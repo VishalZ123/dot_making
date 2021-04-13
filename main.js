64a36ed3c232bd4521dc4ccfd2f50142f24adb38
Vishal <yadav.40@iitj.ac.in>
2021-04-13 18:46:44 +0530

    remove use of Vue.js, use pure js

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-new Vue({
-    el: "#entire",
-    data: {
-        visible: false
-    },
-    methods:{
-        scatter: function(event){
+function scatter(event){
             var tag = document.createElement("div");
             var element = document.getElementById("entire");
             element.appendChild(tag);
             tag.setAttribute("class", "fragments");
             tag.style.top= event.clientY;
             tag.style.left= event.clientX;
-            }
-        }
-});
+            };
